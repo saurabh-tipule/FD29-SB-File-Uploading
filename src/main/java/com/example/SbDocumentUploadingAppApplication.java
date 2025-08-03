aa4d7d1bbc750efa050624dadace6ceb4d5058d3
saurabh-tipule <tipulesaurabh12@gmail.com>
2025-08-03 08:00:16 +0530

    m1() Method Added

--- src/main/java/com/example/SbDocumentUploadingAppApplication.java
+++ src/main/java/com/example/SbDocumentUploadingAppApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbDocumentUploadingAppApplication {
 
+        public void m1()
+        {
+          
+        }
+
 	public static void main(String[] args) {
 		SpringApplication.run(SbDocumentUploadingAppApplication.class, args);
 	}
